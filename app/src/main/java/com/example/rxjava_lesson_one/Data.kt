6fbaf3c4a5ad51f0443a655af00b23916c033468
Slavik Urdzik <urdziksv@gmail.com>
2020-02-07 07:39:03 +0200

    RxJava = Retrofit

---
+++ app/src/main/java/com/example/rxjava_lesson_one/Data.kt
@@ -0,0 +1,29 @@
+package com.example.rxjava_lesson_one
+
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+data class Data(
+    val id: Long,
+    val time: String,
+    val text: String,
+    val image: String
+)
+
+interface WebApi {
+    @GET("messages{page}.json")
+    fun massage(@Path("page") page: Int): Observable<List<Data>>
+}
+
+object retrofitObject {
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .baseUrl("https://rawgit.com/startandroid/data/master/messages/")
+        .build()
+    val retrofitService: WebApi = retrofit.create(WebApi::class.java)
+}
--- app/src/main/java/com/example/rxjava_lesson_one/MainActivity.kt
+++ app/src/main/java/com/example/rxjava_lesson_one/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.rxjava_lesson_one
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.Observable
-import io.reactivex.ObservableOnSubscribe
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Function
 import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
 
 
 //    private val filterOnly: Predicate<String> = Predicate { t ->
@@ -24,7 +20,7 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-
+    private val data = retrofitObject
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -104,56 +100,94 @@ class MainActivity : AppCompatActivity() {
 //        }, 7500)
 //    }
 
-        val observer = object : Observer<Int?> {
-            override fun onComplete() {
-                log("observer completed")
-            }
+//        val observer = object : Observer<Int?> {
+//            override fun onComplete() {
+//                log("observer completed")
+//            }
+//
+//            override fun onSubscribe(d: Disposable) {
+//
+//            }
+//
+//            override fun onNext(t: Int) {
+//                log("onNext value = $t")
+//            }
+//
+//            override fun onError(e: Throwable) {
+//
+//            }
+//        }
+//
+//        val func = Function<Int?, Int?> { t ->
+//            log("func $t")
+//            t * 10
+//        }
+//
+//        val onSubscribe = ObservableOnSubscribe<Int?> { emitter ->
+//            log("call")
+//            for (i in 0..2) {
+//                try {
+//                    TimeUnit.MILLISECONDS.sleep(1000)
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//                emitter.onNext(i)
+//            }
+//            emitter.onComplete()
+//        }
+//
+//        val observable: Observable<Int?> = Observable
+//            .create(onSubscribe)
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(Schedulers.computation())
+//            .map(func)
+//            .observeOn(AndroidSchedulers.mainThread())
+//
+//        log("subscribe")
+//        observable.subscribe(observer)
+//
+//        log("done")
+//
+//
+//    }
 
-            override fun onSubscribe(d: Disposable) {
 
-            }
+//        val stringData: Observable<String> = Observable.just("1", "2", "a", "4", "5")
+//
+//        val observable: Observable<Long> = stringData.map { t -> t.toLong() }
+//            .subscribeOn(Schedulers.io())
+//            .retry(2)
+//
 
-            override fun onNext(t: Int) {
-                log("onNext value = $t")
-            }
 
-            override fun onError(e: Throwable) {
+        val observable = data.retrofitService.massage(1)
 
-            }
-        }
-
-        val func = Function<Int?, Int?> { t ->
-            log("func $t")
-            t * 10
-        }
-
-        val onSubscribe = ObservableOnSubscribe<Int?> { emitter ->
-            log("call")
-            for (i in 0..2) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(1000)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                }
-                emitter.onNext(i)
-            }
-            emitter.onComplete()
-        }
-
-        val observable: Observable<Int?> = Observable
-            .create(onSubscribe)
+        observable
             .subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.computation())
-            .map(func)
             .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<List<Data>> {
 
-        log("subscribe")
-        observable.subscribe(observer)
+                override fun onSubscribe(d: Disposable) {
+                    log("onSubscribe")
+                }
 
-        log("done")
 
+                override fun onComplete() {
+                    log("onComplete")
+                }
+
+                override fun onError(e: Throwable) {
+                    log("Exception $e")
+                }
+
+                override fun onNext(t: List<Data>) {
+                    log("onNext ${t.size}")
+                }
+            })
 
     }
+
+
 }
 
 
