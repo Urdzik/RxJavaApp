e8ee192a151ddbf5129f90da00794ac658f4df3e
Slavik Urdzik <urdziksv@gmail.com>
2020-02-09 08:38:55 +0200

    Started creating App using RxJava

--- app/src/main/java/com/example/rxjava_lesson_one/Data.kt
+++ app/src/main/java/com/example/rxjava_lesson_one/Data.kt
@@ -8,22 +8,32 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 data class Data(
-    val id: Long,
-    val time: String,
-    val text: String,
-    val image: String
+    val title : String,
+    val text : String,
+    val author : String,
+    val ganre : String,
+    val volume : String,
+    val date : String,
+    val url : String,
+    val rating : Rating
+)
+
+data class Rating (
+
+    val litres : Double,
+    val liveLib : Double
 )
 
 interface WebApi {
-    @GET("messages{page}.json")
-    fun massage(@Path("page") page: Int): Flowable<List<Data>>
+    @GET("db.json")
+    fun massage() : Flowable<List<Data>>
 }
 
 object retrofitObject {
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .baseUrl("https://rawgit.com/startandroid/data/master/messages/")
+        .baseUrl("https://raw.githubusercontent.com/Urdzik/dataBase/master/")
         .build()
     val retrofitService: WebApi = retrofit.create(WebApi::class.java)
 }
--- app/src/main/java/com/example/rxjava_lesson_one/MainActivity.kt
+++ app/src/main/java/com/example/rxjava_lesson_one/MainActivity.kt
@@ -7,182 +7,36 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subscribers.DisposableSubscriber
-
-
-//    private val filterOnly: Predicate<String> = Predicate { t ->
-//        t.contains("5")
-//    }
-//
-//    var zipIntWithString: BiFunction<Int?, String?, String?> = BiFunction { t1, t2 ->
-//        "$t1: $t2"
-//    }
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val data = retrofitObject
-    lateinit var disposable: Disposable
+    private lateinit var disposable: Disposable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val observable: Observable<Long> = Observable.interval(1, TimeUnit.SECONDS)
-//
-//        val observer: Consumer<Long> = Consumer { t ->
-//            Log.d("One2", "$t")
-//        }
-//        val subscription = observable.subscribe(observer)
-//
-//        window.decorView.postDelayed({
-//            Log.d("One2", "unsubscribe")
-//            subscription.dispose()
-//        }, 4500)
 
-//        val observer1 = object : Observer<Long> {
-//            override fun onComplete() {
-//                log("observer1 onComplete")
-//            }
-//
-//            override fun onSubscribe(d: Disposable) {
-//
-//            }
-//
-//            override fun onNext(t: Long) {
-//                log("observer1 onNext $t")
-//            }
-//
-//            override fun onError(e: Throwable) {
-//
-//            }
-//
-//        }
-//
-//        val observer2 = object : Observer<Long> {
-//            override fun onSubscribe(d: Disposable) {
-//
-//            }
-//
-//            override fun onComplete() {
-//                log("observer2 onComplete")
-//
-//            }
-//
-//            override fun onNext(t: Long) {
-//                log("observer2 onNext $t")
-//            }
-//
-//            override fun onError(e: Throwable) {
-//
-//            }
-//
-//        }
-//
-//        val observable = Observable.interval(1, TimeUnit.SECONDS).take(10)
-//
-//        val subject: UnicastSubject<Long> = UnicastSubject.create()
-//
-//        log("subject subscribe")
-//        observable.subscribe(subject)
-//
-//
-//
-//        Handler().postDelayed({
-//            log("observer1 subscribe")
-//            subject.subscribe(observer1)
-//        }, 3500)
-//
-//        Handler().postDelayed({
-//            log("observer2 subscribe")
-//            subject.subscribe(observer2)
-//        }, 5500)
-//
-//        window.decorView.postDelayed({
-//            subject.onNext(100L)
-//        }, 7500)
-//    }
-
-//        val observer = object : Observer<Int?> {
-//            override fun onComplete() {
-//                log("observer completed")
-//            }
-//
-//            override fun onSubscribe(d: Disposable) {
-//
-//            }
-//
-//            override fun onNext(t: Int) {
-//                log("onNext value = $t")
-//            }
-//
-//            override fun onError(e: Throwable) {
-//
-//            }
-//        }
-//
-//        val func = Function<Int?, Int?> { t ->
-//            log("func $t")
-//            t * 10
-//        }
-//
-//        val onSubscribe = ObservableOnSubscribe<Int?> { emitter ->
-//            log("call")
-//            for (i in 0..2) {
-//                try {
-//                    TimeUnit.MILLISECONDS.sleep(1000)
-//                } catch (e: InterruptedException) {
-//                    e.printStackTrace()
-//                }
-//                emitter.onNext(i)
-//            }
-//            emitter.onComplete()
-//        }
-//
-//        val observable: Observable<Int?> = Observable
-//            .create(onSubscribe)
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(Schedulers.computation())
-//            .map(func)
-//            .observeOn(AndroidSchedulers.mainThread())
-//
-//        log("subscribe")
-//        observable.subscribe(observer)
-//
-//        log("done")
-//
-//
-//    }
-
-
-//        val stringData: Observable<String> = Observable.just("1", "2", "a", "4", "5")
-//
-//        val observable: Observable<Long> = stringData.map { t -> t.toLong() }
-//            .subscribeOn(Schedulers.io())
-//            .retry(2)
-//
-
-
-        val flowable = data.retrofitService.massage(1)
+        val flowable = data.retrofitService.massage()
         disposable = flowable
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeWith(object : DisposableSubscriber<List<Data>>() {
                 override fun onComplete() {
                     log("onComplete")
-
                 }
 
                 override fun onNext(t: List<Data>) {
-                            log("onNext ${t.size}") }
+                        tv.text = t.toString()
+                }
 
                 override fun onError(t: Throwable?) {
                     log("$t")
                 }
 
-
-
-
             })
-
     }
 
     override fun onPause() {
@@ -190,10 +44,7 @@ class MainActivity : AppCompatActivity() {
         log("Dispose")
         disposable.dispose()
     }
-
-
 }
-
 
 fun log(s: String) {
     Log.d("one2", s)
