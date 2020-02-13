7f70c3a2c176b25300a1fd3f6e7085b9238a10e4
Slavik Urdzik <Urdziksv@gmail.com>
2020-02-13 08:24:02 +0200

    Added new Activity Added RxBinding Listener

--- app/src/main/java/com/example/rxjava_lesson_one/EditTextActivity.kt
+++ app/src/main/java/com/example/rxjava_lesson_one/EditTextActivity.kt
@@ -15,5 +15,41 @@ class EditTextActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_text)
 
+//       fun sub() = nameEditText.textChanges().subscribe {
+//           textView3.text = nameEditText.text.toString()
+//       }
+        s()
+
+    }
+
+    @SuppressLint("CheckResult")
+    fun s() {
+        val name =
+            RxTextView
+                .textChanges(nameEditText)
+                .map { t -> t.length > 2 }
+                .distinctUntilChanged()
+
+        name
+            .map { b -> if (b) Color.BLACK else Color.RED }
+            .subscribe { c -> nameEditText.setTextColor(c) }
+
+        val age =
+            RxTextView
+                .textChanges(ageEditText)
+                .map { t -> t.length > 6 }
+                .distinctUntilChanged()
+        age
+            .map { b -> if (b) Color.BLACK else Color.RED }
+            .subscribe { c -> ageEditText.setTextColor(c) }
+
+        val isSingUpPossible = Observables
+            .combineLatest(name, age) { e, p -> e && p }
+            .distinctUntilChanged()
+
+        isSingUpPossible.subscribe { b ->
+            button.isEnabled = b
+            button.isClickable = b
+        }
     }
 }
